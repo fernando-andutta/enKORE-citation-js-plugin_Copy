998e993d3e3fb2d59d6aaae130cfcf057bfed2ef
Andutta <44423566+fernando-andutta@users.noreply.github.com>
2023-01-28 03:56:12 +1000

    Add files via upload

---
+++ src/id.js
@@ -0,0 +1,21 @@
+import wdk from "wikidata-sdk";
+import config from "./config.json" assert { type: "json" };
+
+/**
+ * Get Wikidata JSON from Wikidata IDs
+ *
+ * @access protected
+ * @method parse
+ * @memberof module:@citation-js/plugin-wikidata.parsers.id
+ *
+ * @param {String|Array<String>} data - Wikidata IDs
+ * @param {Array<String>} [langs]
+ *
+ * @return {Object} Wikidata JSON
+ */
+function parseWikidata(data, langs) {
+	const list = [].concat(data);
+	return [].concat(wdk.getManyEntities(list, langs || config.langs));
+}
+
+export { parseWikidata as parse, parseWikidata as default };
